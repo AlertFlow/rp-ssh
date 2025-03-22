bea03668349ed1cce5ec7892aa3bafe50cd7458b
Justin Neubert <neubert_justin@outlook.com>
2025-03-22 11:45:05 +0100

    Update dependencies and increment plugin version to 1.1.1

--- main.go
+++ main.go
@@ -320,11 +320,11 @@ func (p *Plugin) EndpointRequest(request plugins.EndpointRequest) (plugins.Respo
 	}, errors.New("not implemented")
 }
 
-func (p *Plugin) Info() (models.Plugin, error) {
+func (p *Plugin) Info(request plugins.InfoRequest) (models.Plugin, error) {
 	var plugin = models.Plugin{
 		Name:    "SSH",
 		Type:    "action",
-		Version: "1.1.0",
+		Version: "1.1.1",
 		Author:  "JustNZ",
 		Action: models.Action{
 			Name:        "SSH",
@@ -428,8 +428,8 @@ func (s *PluginRPCServer) EndpointRequest(request plugins.EndpointRequest, resp 
 	return err
 }
 
-func (s *PluginRPCServer) Info(args interface{}, resp *models.Plugin) error {
-	result, err := s.Impl.Info()
+func (s *PluginRPCServer) Info(request plugins.InfoRequest, resp *models.Plugin) error {
+	result, err := s.Impl.Info(request)
 	*resp = result
 	return err
 }
